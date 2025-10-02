6a2b9a9e7134f2b4c1b7769f61bc585554b1ff3e
Ravi Saini <thisisravisaini@gmail.com>
2025-10-02 16:24:52 +0530

    Added Test Case

--- src/test/java/responsebody/UserResponseBody.java
+++ src/test/java/responsebody/UserResponseBody.java
@@ -1,4 +1,41 @@
 package responsebody;
 
 public class UserResponseBody {
+
+    int code;
+    String status;
+    String message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponseBody{" +
+                "code=" + code +
+                ", status='" + status + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
--- src/test/java/tests/UserTest.java
+++ src/test/java/tests/UserTest.java
@@ -1,15 +1,21 @@
 package tests;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import endpoints.UserEndPoints;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import requestbody.UserRequestBody;
+import responsebody.UserResponseBody;
 
 public class UserTest {
 
-    public static UserRequestBody userRequestBody;
+    SoftAssert softAssert = new SoftAssert();
+
+    UserRequestBody userRequestBody;
 
     @BeforeClass
     public UserRequestBody createUserTestData() {
@@ -26,7 +32,22 @@ public class UserTest {
 
 
     @Test
-    public static void createUSerTest() {
+    public void createUserTest() throws JsonProcessingException {
         Response response = UserEndPoints.createUser(userRequestBody);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        UserResponseBody userResponseBody = objectMapper.readValue(response.asString(), UserResponseBody.class);
+
+        System.out.println(userResponseBody.getCode());
+        System.out.println(userResponseBody.getStatus());
+        System.out.println(userResponseBody.getMessage());
+
+        softAssert.assertEquals(userResponseBody.getCode(), "200");
+        softAssert.assertEquals(userResponseBody.getStatus(), "Success");
+        softAssert.assertEquals(userResponseBody.getMessage(), "User created successfully");
+
+        softAssert.assertAll();
+
+
     }
 }
