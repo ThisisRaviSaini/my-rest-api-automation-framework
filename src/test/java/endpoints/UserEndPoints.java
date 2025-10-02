b237371e7255c5a4e24bcf79d9baf125cb7ed513
Ravi Saini <thisisravisaini@gmail.com>
2025-10-02 15:38:29 +0530

    Added Test

--- src/test/java/endpoints/UserEndPoints.java
+++ src/test/java/endpoints/UserEndPoints.java
@@ -1,4 +1,24 @@
 package endpoints;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import requestbody.UserRequestBody;
+
+import static io.restassured.RestAssured.given;
+
 public class UserEndPoints {
+
+
+    public static Response createUser(UserRequestBody userRequestBody){
+
+        Response response= given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON).body(userRequestBody).when().post(Routes.postUrl);
+        return response;
+    }
+
+    public static Response getUser(String username){
+
+        Response response= given().pathParam("username", username).when().get(Routes.getUrl);
+        return response;
+    }
 }
--- src/test/java/tests/UserTest.java
+++ src/test/java/tests/UserTest.java
@@ -1,4 +1,32 @@
 package tests;
 
+import com.github.javafaker.Faker;
+import endpoints.UserEndPoints;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import requestbody.UserRequestBody;
+
 public class UserTest {
+
+    public static UserRequestBody userRequestBody;
+
+    @BeforeClass
+    public UserRequestBody createUserTestData() {
+        UserRequestBody userRequestBody = new UserRequestBody();
+        Faker faker = new Faker();
+
+        userRequestBody.setId(faker.idNumber().hashCode());
+        userRequestBody.setFirstName(faker.name().firstName());
+        userRequestBody.setLastName(faker.name().lastName());
+        userRequestBody.setCompany(faker.company().name());
+
+        return userRequestBody;
+    }
+
+
+    @Test
+    public static void createUSerTest() {
+        Response response = UserEndPoints.createUser(userRequestBody);
+    }
 }
